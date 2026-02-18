a6c904e2cca1128f0d2b54a09f8accf257235e5c
brunohuguenin <brunohugueninn@gmail.com>
2026-02-18 16:56:03 -0300

    Exercício 01
    Prática sobre o assunto de Listas

---
+++ Aula08/listas/ex01.py
@@ -0,0 +1,10 @@
+# Crie uma lista de números inteiros e use um for para calcular a soma de todos os elementos (sem usar sum()).
+
+num = [1,2,3,4,5,6,7,8,9]
+
+soma_total = 0
+for i in num:
+  somaTotal += i
+
+print(f"A lista de números intiros: {num}")
+print(f"A soma de todos os elementos é: {soma_total}")
