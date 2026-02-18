f911e76a4907d8f66f00c09e2a15317c8be780a7
brunohuguenin <brunohugueninn@gmail.com>
2026-02-18 18:14:45 -0300

    Exercício 01
    Prática sobre tuplas

---
+++ Aula08/tuplas/ex01.py
@@ -0,0 +1,11 @@
+# Crie uma tupla com 5 números inteiros e use um for para imprimir todos os valores.
+
+val = (1,5,10,15,20)
+
+for i in val:
+  print(val)
+
+print()
+
+for i in range(len(val)):
+  print(val[i])
