d157590faf253e09aff987993e3ea6bdd3b01db8
brunohuguenin <brunohugueninn@gmail.com>
2026-02-21 13:11:10 -0300

    Atualização do código

--- Aula08/tuplas/ex05.py
+++ Aula08/tuplas/ex05.py
@@ -8,5 +8,5 @@ for i in val:
   if valor_na_tupla == i:
     print(f"O valor {valor_na_tupla} se encontra na tupla na posição: {val.index(i)}")
     break
-  else:
-    print(f"O valor {valor_na_tupla} não se encontra na tupla")
+else:
+  print(f"O valor {valor_na_tupla} não se encontra na tupla")
